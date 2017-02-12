d9ecf9fe29ff8aae2db009b9badb556f247b2d10
Trey Tabner <trey@tabner.com>
2017-02-12 12:09:07 -0600

    Initial upload

---
+++ web-hostname.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		hostname, _ := os.Hostname()
+		io.WriteString(w, hostname)
+	})
+	http.ListenAndServe(":9000", nil)
+}
